2a156c98bc969e612f6aad63fc88575733fca801
Chris O'Brien <irishsoxmax@gmail.com>
2017-01-31 02:05:02 +0000

    add in all materials and prices

--- app/myControl.js
+++ app/myControl.js
@@ -1,12 +1,21 @@
 /*global app*/
 
 function IndustryMaterial(name, price) {
-    this.name = name;
-    this.price = price;
+    return {
+        name: name,
+        price: price
+    }
 }
 
 app.controller("myCtrl", function($scope) {
-    $scope.ores = [new IndustryMaterial("Veldspar", 1), new IndustryMaterial("Scordite", 2), new IndustryMaterial("Plagioclase", 3)];
+    $scope.ores = [IndustryMaterial("Spodumain", 3700), IndustryMaterial("Bistot", 5100), IndustryMaterial("Crokite", 3700), IndustryMaterial("Arkonor", 4400), IndustryMaterial("Gneiss", 1200),
+        IndustryMaterial("Gneiss", 4400), IndustryMaterial("Dark Ochre", 1300), IndustryMaterial("Mercoxit", 21000), IndustryMaterial("Bacteria", 340), IndustryMaterial("Electrolytes", 390),
+        IndustryMaterial("Oxygen", 305), IndustryMaterial("Plasmoids", 280), IndustryMaterial("Water", 415), IndustryMaterial("Synthetic Oil", 8000), IndustryMaterial("Superconductors", 9000),
+        IndustryMaterial("Oxides", 10000), IndustryMaterial("Coolant", 10500), IndustryMaterial("Mechanical Parts", 10000), IndustryMaterial("Consumer Electronics", 8000), IndustryMaterial("Water-Cooled CPU", 9000),
+        IndustryMaterial("Nanites", 6000), IndustryMaterial("Livestock", 9500), IndustryMaterial("Construction Blocks", 8500), IndustryMaterial("Test Cultures", 10000), IndustryMaterial("Fertilizer", 11000),
+        IndustryMaterial("Ukomi Superconductors", 65000), IndustryMaterial("Robotics", 80000), IndustryMaterial("Condensates", 55000), IndustryMaterial("Supercomputers", 105000), IndustryMaterial("Biotech Research Reports", 108000),
+        IndustryMaterial("Cryoprotectant Solution", 115000), IndustryMaterial("Organic Mortar Applicators", 1000000), IndustryMaterial("Wetware Mainframe", 2400000)
+    ];
     $scope.units = {};
     $scope.ores.forEach(function(ore) {
         $scope.units[ore.name] = 0;
@@ -15,8 +24,6 @@ app.controller("myCtrl", function($scope) {
         var total = 0;
         for (const ore of $scope.ores) {
             total += ore.price * $scope.units[ore.name];
-            console.log(ore.name)
-            console.log($scope.units[ore.name])
         }
         return total;
     };
