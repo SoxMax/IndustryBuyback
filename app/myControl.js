29969efbb35ddfb23ce93b5d59bc5b406d064da7
Chris O'Brien <irishsoxmax@gmail.com>
2017-01-31 01:18:05 +0000

    fix total output

--- app/myControl.js
+++ app/myControl.js
@@ -1,10 +1,23 @@
 /*global app*/
 
 function IndustryMaterial(name, price) {
-    this.Name = name;
-    this.Price = price;
+    this.name = name;
+    this.price = price;
 }
 
 app.controller("myCtrl", function($scope) {
     $scope.ores = [new IndustryMaterial("Veldspar", 1), new IndustryMaterial("Scordite", 2), new IndustryMaterial("Plagioclase", 3)];
+    $scope.units = {};
+    $scope.ores.forEach(function(ore) {
+        $scope.units[ore.name] = 0;
+    });
+    $scope.totalPrice = function() {
+        var total = 0;
+        for (const ore of $scope.ores) {
+            total += ore.price * $scope.units[ore.name];
+            console.log(ore.name)
+            console.log($scope.units[ore.name])
+        }
+        return total;
+    };
 });
