6d5d6ec37cc71c446de71b6f95299de49cfb754e
Chris O'Brien <irishsoxmax@gmail.com>
2017-02-20 23:14:30 +0000

    working data load from google sheets

--- app/myControl.js
+++ app/myControl.js
@@ -1,4 +1,4 @@
-/*global app*/
+/*global app gapi*/
 
 function IndustryMaterial(name, price) {
     return {
@@ -8,18 +8,34 @@ function IndustryMaterial(name, price) {
 }
 
 app.controller("myCtrl", function($scope) {
-    $scope.ores = [IndustryMaterial("Spodumain", 3700), IndustryMaterial("Bistot", 5100), IndustryMaterial("Crokite", 3700), IndustryMaterial("Arkonor", 4400), IndustryMaterial("Gneiss", 1200),
-        IndustryMaterial("Gneiss", 4400), IndustryMaterial("Dark Ochre", 1300), IndustryMaterial("Mercoxit", 21000), IndustryMaterial("Bacteria", 340), IndustryMaterial("Electrolytes", 390),
-        IndustryMaterial("Oxygen", 305), IndustryMaterial("Plasmoids", 280), IndustryMaterial("Water", 415), IndustryMaterial("Synthetic Oil", 8000), IndustryMaterial("Superconductors", 9000),
-        IndustryMaterial("Oxides", 10000), IndustryMaterial("Coolant", 10500), IndustryMaterial("Mechanical Parts", 10000), IndustryMaterial("Consumer Electronics", 8000), IndustryMaterial("Water-Cooled CPU", 9000),
-        IndustryMaterial("Nanites", 6000), IndustryMaterial("Livestock", 9500), IndustryMaterial("Construction Blocks", 8500), IndustryMaterial("Test Cultures", 10000), IndustryMaterial("Fertilizer", 11000),
-        IndustryMaterial("Ukomi Superconductors", 65000), IndustryMaterial("Robotics", 80000), IndustryMaterial("Condensates", 55000), IndustryMaterial("Supercomputers", 105000), IndustryMaterial("Biotech Research Reports", 108000),
-        IndustryMaterial("Cryoprotectant Solution", 115000), IndustryMaterial("Organic Mortar Applicators", 1000000), IndustryMaterial("Wetware Mainframe", 2400000)
-    ];
+
+    function initClient() {
+        // Initialize the JavaScript client library.
+        gapi.client.init({
+            'apiKey': 'AIzaSyBKZwtEyuUFaLne6VybUZY8YGZ9n8BA9d8',
+            'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4']
+        }).then(function() {
+            gapi.client.sheets.spreadsheets.values.get({
+                spreadsheetId: '1chAdg92S0KEXY9zlKDwWI5ft2w8ygJBDSgWk-JxUrpw',
+                range: 'Materials!A2:B',
+                valueRenderOption: 'UNFORMATTED_VALUE'
+            }).then(function(response) {
+                var materials = [];
+                for (const row of response.result.values) {
+                    materials.push(IndustryMaterial(row[0], row[1]));
+                }
+                $scope.ores = materials;
+                $scope.ores.forEach(function(ore) {
+                    $scope.units[ore.name] = 0;
+                });
+                $scope.$apply();
+            });
+        });
+    };
+    gapi.load('client', initClient);
+
+    $scope.ores = [];
     $scope.units = {};
-    $scope.ores.forEach(function(ore) {
-        $scope.units[ore.name] = 0;
-    });
     $scope.totalPrice = function() {
         var total = 0;
         for (const ore of $scope.ores) {
